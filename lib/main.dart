3f2fcd3e6738d77b626fc5ad6a3436912b82c41d
AyaGh <eng.aya93m@gmail.com>
2021-07-08 12:02:12 +0300

    first modefing

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  //final _auth = Auth();
+  final _auth = Auth();
 
   @override
   Widget build(BuildContext context) {
